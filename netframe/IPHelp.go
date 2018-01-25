e0668b82750ac0ab2a3eb99fb7a24b8e37a31afd
panh1@sohu.com <panh1@sohu.com>
2018-01-25 15:05:38 +0800

    modify

---
+++ netframe/IPHelp.go
@@ -0,0 +1,45 @@
+package netframe
+
+import (
+	"bytes"
+	"strconv"
+)
+
+//网络字节序转IP
+func NetIpIntToString(ipInt int) string {
+	ipSegs := make([]string, 4)
+	var len int = len(ipSegs)
+	buffer := bytes.NewBufferString("")
+	for i := 0; i < len; i++ {
+		tempInt := ipInt & 0xFF
+		ipSegs[i] = strconv.Itoa(tempInt)
+		ipInt = ipInt >> 8
+	}
+	for i := 0; i < len; i++ {
+		buffer.WriteString(ipSegs[i])
+		if i < len-1 {
+			buffer.WriteString(".")
+		}
+	}
+	return buffer.String()
+}
+
+//主机字节序转IP
+func HostIpIntToString(ipInt int) string {
+	ipSegs := make([]string, 4)
+	var len int = len(ipSegs)
+	buffer := bytes.NewBufferString("")
+	for i := 0; i < len; i++ {
+		tempInt := ipInt & 0xFF
+		ipSegs[len-i-1] = strconv.Itoa(tempInt)
+		ipInt = ipInt >> 8
+	}
+	for i := 0; i < len; i++ {
+		buffer.WriteString(ipSegs[i])
+		if i < len-1 {
+			buffer.WriteString(".")
+		}
+	}
+	return buffer.String()
+}
+
--- netframe/netframe.go
+++ netframe/netframe.go
@@ -3,6 +3,7 @@ package netframe
 import (
 	"encoding/binary"
 	"bytes"
+	"github.com/golang/protobuf/proto"
 )
 
 var g_hh = []byte{0x03, 0x02}
@@ -80,4 +81,95 @@ func (frame *NetFrame) Unpack(data []byte) (bool, int) {
 	}
 
 	return result, datasize
+}
+
+type PbNetFrame struct {
+	data []byte
+}
+
+func (pack *PbNetFrame) Pack(head, body proto.Message) (data []byte, l int) {
+	headBuf, err := proto.Marshal(head)
+	if err != nil {
+		l = 0
+		return
+	}
+	bodyBuf, err := proto.Marshal(body)
+	if err != nil {
+		l = 0
+		return
+	}
+
+	dataBuf := new(bytes.Buffer)
+	binary.Write(dataBuf, binary.BigEndian, byte('('))
+	binary.Write(dataBuf, binary.BigEndian, int32(len(headBuf)))
+	binary.Write(dataBuf, binary.BigEndian, int32(len(bodyBuf)))
+	binary.Write(dataBuf, binary.BigEndian, headBuf)
+	binary.Write(dataBuf, binary.BigEndian, bodyBuf)
+	binary.Write(dataBuf, binary.BigEndian, byte(')'))
+	data = dataBuf.Bytes()
+	l = dataBuf.Len()
+	return
+}
+
+func (pack *PbNetFrame) Append(data []byte) {
+	pack.data = append(pack.data, data...)
+}
+func (pack *PbNetFrame) UnPack(head, body proto.Message) (ret bool) {
+	if len(pack.data) < 10 {
+		ret = false
+		return
+	}
+
+	ret = false
+	buf := bytes.NewBuffer(pack.data)
+	pack.data = pack.data[:0]
+	for {
+		var hh byte
+		err := binary.Read(buf, binary.BigEndian, &hh)
+		if err != nil || hh != byte('(') {
+			continue
+		}
+
+		var hhl int32
+		err = binary.Read(buf, binary.BigEndian, &hhl)
+		if err != nil {
+			continue
+		}
+
+		var ttl int32
+		err = binary.Read(buf, binary.BigEndian, &ttl)
+		if err != nil {
+			continue
+		}
+
+		headData := make([]byte, hhl)
+		err = binary.Read(buf, binary.BigEndian, &headData)
+		if err != nil {
+			continue
+		}
+
+		bodyData := make([]byte, ttl)
+		err = binary.Read(buf, binary.BigEndian, &bodyData)
+		if err != nil {
+			continue
+		}
+
+		var tt byte
+		err = binary.Read(buf, binary.BigEndian, &tt)
+		if err != nil || tt != byte(')') {
+			continue
+		}
+		err = proto.Unmarshal(headData, head)
+		if err != nil {
+			continue
+		}
+		err = proto.Unmarshal(bodyData, body)
+		if err != nil {
+			continue
+		}
+		ret = true
+		break
+	}
+	pack.data = append(pack.data, buf.Bytes()...)
+	return
 }
