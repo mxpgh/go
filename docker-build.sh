c8efdefd4f63c297319dd78c28d9699e8c58a986
mxpgh <panh1@sohu.com>
2019-07-18 13:43:28 +0800

    Add files via upload

--- docker-build.sh
+++ docker-build.sh
@@ -2,19 +2,24 @@
 basepath=$(cd `dirname $0`; pwd)
 echo "current path: ${basepath}"
 
-cl=$(docker ps -a | grep basic_img-arm:1.0 | awk '{print $1}')
-if [ ! ${cl} ]; then
-   echo
-else
-    docker stop ${cl}
-    docker rm ${cl}
+img1=$(docker images | grep basic_img-arm| awk '{if($2=="1.0"){print $3}}')
+if [ -n "${img1}" ]; then
+    c1=$(docker ps -a | grep ${img1} | awk '{print $1}')
+    if [ -n "${cl}" ]; then
+        docker stop ${cl}
+        docker rm ${cl}
+    fi
 fi
 
-img=$(docker images | grep basic_img-arm | awk '{print $3}')
-if [ ! ${img} ]; then
-   echo
-else
-    docker rmi ${img}
+c2=$(docker ps -a | grep basic_img-arm:1.0 | awk '{print $1}')
+if [ -n "${c2}" ]]; then
+    docker stop ${c2}
+    docker rm ${c2}
+fi
+
+img2=$(docker images | grep basic_img-arm | awk '{print $3}')
+if [ -n "${img2}" ]; then
+    docker rmi ${img2}
 fi
 
 chmod a+x $basepath/appctl $basepath/appctl-daemon
--- updimg.sh
+++ updimg.sh
@@ -12,19 +12,24 @@ echo "镜像路径: ${updimg}"
 basepath=$(cd `dirname $0`; pwd)
 echo "current path: ${basepath}"
 
-cl=$(docker ps -a | grep basic_img-arm:1.0 | awk '{print $1}')
-if [ ! ${cl} ]; then
-   echo
-else
-    docker stop ${cl}
-    docker rm ${cl}
+img1=$(docker images | grep basic_img-arm| awk '{if($2=="1.0"){print $3}}')
+if [ -n "${img1}" ]; then
+    c1=$(docker ps -a | grep ${img1} | awk '{print $1}')
+    if [ -n "${cl}" ]; then
+        docker stop ${cl}
+        docker rm ${cl}
+    fi
 fi
 
-img=$(docker images | grep basic_img-arm | awk '{print $3}')
-if [ ! ${img} ]; then
-   echo
-else
-    docker rmi ${img}
+c2=$(docker ps -a | grep basic_img-arm:1.0 | awk '{print $1}')
+if [ -n "${c2}" ]]; then
+    docker stop ${c2}
+    docker rm ${c2}
+fi
+
+img2=$(docker images | grep basic_img-arm | awk '{print $3}')
+if [ -n "${img2}" ]; then
+    docker rmi ${img2}
 fi
 
 docker load < ${updimg}
@@ -32,6 +37,7 @@ docker run -idt --name app-ctl-test basic_img-arm:1.0
 imgv=$(docker exec app-ctl-test appctl -version container)
 docker stop app-ctl-test
 docker rm app-ctl-test
+
 echo ""
 echo "镜像更新成功，当前版本: ${imgv}"
 echo "更新完成."
